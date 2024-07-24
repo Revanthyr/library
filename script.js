40002c4962e0028116cbec42a5c37653f33eee84
Revanthyr <155083567+Revanthyr@users.noreply.github.com>
2024-07-24 16:18:09 +0200

    Add styling to book cards, add form

--- script.js
+++ script.js
@@ -16,11 +16,17 @@ const book1 = new Book("Cool book","cool guy",42,false);
 addBooktoLibrary(book1);
 console.log(library);
 const book2 = new Book("Test","fr test",100000,true);
-const book3 = new Book("Test","fr test",100000,true);
+const book3 = new Book("Test"," test",100000,true);
+const book4 = new Book("Test","fr tedzqdzqst",100000,true);
 addBooktoLibrary(book2);
 addBooktoLibrary(book3);
+addBooktoLibrary(book4);
 
-
+let removeButtonId = 0;
+let itemToRemove = {
+ 
+};
+let testcar = 5;
 function showBooks(){
     
     library.map((book)=>{
@@ -29,58 +35,101 @@ function showBooks(){
         const docTitle =document.createElement("p");
         const docAuthor = document.createElement("p");
         const docPages = document.createElement("p");
-        const docIsRead = document.createElement("p");
+        const docIsRead = document.createElement("button");
+        const removeButton = document.createElement("button");
         body.appendChild(div);
         div.appendChild(docTitle);
         docTitle.textContent = book.title;
         div.appendChild(docAuthor);
         docAuthor.textContent=book.author;
         div.appendChild(docPages);
-        docPages.textContent = book.pagesNum.toString();
+        docPages.textContent = book.pagesNum.toString() + " pages";
         div.appendChild(docIsRead);
-        docIsRead.textContent = book.isRead;
+        if (book.isRead ==true){
+            docIsRead.textContent = "Read";
+            docIsRead.classList.add("read");
+        }
+        else {
+            docIsRead.textContent = "Not Read... Yet!";
+            docIsRead.classList.add("notread");
+        }
+        docIsRead.addEventListener("click",()=>{
+            if (docIsRead.classList.contains("read")){
+                docIsRead.classList.remove("read");
+                docIsRead.classList.add("notread");
+                docIsRead.textContent = "Not Read... Yet!"
+            }
+            else if (docIsRead.classList.contains("notread")){
+                docIsRead.classList.remove("notread");
+                docIsRead.classList.add("read");
+                docIsRead.textContent = "Read";
+            }
+        })
+        div.appendChild(removeButton);
+        div.setAttribute("bookNumber",library.indexOf(book));
+        removeButton.setAttribute("bookNumber",library.indexOf(book));
+        removeButton.classList.add("remove");
+        removeButton.textContent = "Remove";
+        removeButton.addEventListener("click",()=>{
+            body.removeChild(removeButton.parentElement)
+             
+        })
     })
 }
+
+
 showBooks();
+
 
 const addButton = document.querySelector(".add-btn");
 const dialog = document.querySelector("dialog");
 const closeButton = document.querySelector(".close-btn");
-addButton.addEventListener("click",()=>{
-   dialog.showModal();
-})
+
 closeButton.addEventListener("click",()=>{
     dialog.close();
 })
+const page = document.querySelector("body");
+addButton.addEventListener("click",()=>{
+    dialog.showModal();
+})
+   
+
+
 
 
 /*
- THE PAGE
-       :D -add a top bar with the title, (change font) 
-       :D -crazete three flex divs for page layout (or two), change that in the js file for the body.appendCHild
+addButton.addEventListener("click",()=>{
+   dialog.showModal();
+
+})
+     page.addEventListener("click",()=>{
+    if (dialog.getAttribute("open")==""){
+        dialog.close();
+    }
+    //dialog.close();
+})  
+       if (dialog.getAttribute("open")==""){
+    page.addEventListener("click",()=>{
+        dialog.close();
+    })
+}
+else {
+    addButton.addEventListener("click",()=>{
+        dialog.showModal()
+    })
+}
+         
+
+
         
-      :D  -cneter the add book byutton
-       :D - and make it look good (font,border,backgroundcolor,
-       :D -add a class to the book container
-        - use css to style whats inside the book container (font,grid layout)
-
-    book container:
-         :D grid layout
-         -add gap between cardfs
-         -add backdrop to cards
-         -change the font of text and align it center in cards
-         - add read and remove buttons
-         -
-
-
         
-        - add padding to the sides (vw)
         when too many books add scrollbar
         -figure outthe colors 'gpt)
         -change the background color to an image of library?,
         -button hovercolor) 
-        add remove button
-        add read / not read button
+       
+       
+        -add backdrop to cards
  THE DIALOG
         -add popup animation
         -add 3 input with right font and auto text and right types
@@ -91,6 +140,6 @@ closeButton.addEventListener("click",()=>{
         -figure oput positioning
 
 
- 
+            
 
 */
